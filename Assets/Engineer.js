c778b58ff916f17e96194106c140db482ce50918
Lilyfields <lilyfields11@bigpond.com>
2022-02-10 14:31:29 +1100

    updated object js file

--- Assets/Engineer.js
+++ Assets/Engineer.js
@@ -1,5 +1,16 @@
 const Employee = require ('./Employee');
 
 class Engineer extends Employee {
-
+    constructor ({name, id, email,githubUsername}) {
+        super({name,id, email});
+        this.githubUsername =githubUsername;
+        this.role = "Engineer";
+    }
+    getGithubUsername (){
+        return this.githubUsername;
+    }
+    getRole () {
+        return "Engineer";
+    }
 }
+module.exports = Engineer;
--- Assets/Manager.js
+++ Assets/Manager.js
@@ -1,3 +1,18 @@
-class Manager {
-    
+const Employee = require('./Employee')
+
+class Manager extends Employee {
+    constructor({name, id, email,officeNumber}) {
+        super({name,email,id});
+        this.officeNumber = officeNumber;
+        this.role = "Manager";
+    }
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+    getRole () {
+        return "Manager";
+    }
+
 }
+
+module.exports = Manager;
