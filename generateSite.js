3450f2cccbb108c0a11b5b9263e4b57d1ca4015a
Lilyfields <lilyfields11@bigpond.com>
2022-02-13 20:20:21 +1100

    change formatting

--- generateSite.js
+++ generateSite.js
@@ -1,153 +1,68 @@
-const generateTeamProfile = (members) => {
-        console.log(members);
+const fs= require ("fs");
+const path = require ("path");
+const templatesDir = path.resolve(__dirname,"Team Profile Generator");
 
-    //generate teamProfile by calling on the HTML array
-    const HTML = [];
+const render = teamMembers => {
 
+const HTML = [];
 
-    //if Manager was selected from inquirer then this will also get called to generate the managerHtml.
-
-        const buildManager = manager => {
-        console.log(buildManager);
-        let managerHtml = `
-
-        <div class="d-flex justify-content-around flex-wrap mt-5 m-3">
-        <div class="card-header">
-        ${manager.name} <br/>
-        <i class="fa-duotone fa-alicorn"></i> Manager</div>
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${manager.id}</li>
-        <li class="list-group-item">Email:<span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
-        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
-        </ul>
-        </div>
-
-        `;
-        HTML.push(managerHtml);
+HTML.push(...teamMembers
+    .filter(teamMembers =>teamMembers.getRole() === "Manager")
+    .map(manager =>renderManager(manager))
+    );
     
-    }
+HTML.push(...teamMembers
+    .filter(teamMembers=>teamMembers.getRole()==="Engineer")
+    .map(engineer=>renderEngineer(engineer))
+    );
 
-    //if Engineer was selected from inquirer then this will also get called to generate the engineerHtml.
-
-    const buildEngineer = engineer => {
-        console.log(buildEngineer);
-        let engineerHtml = `
-            
-        <div class="d-flex justify-content-around flex-wrap mt-5 m-3">
-        <div class="card-header">
-        ${engineer.name} <br/>
-        <i class="fa-duotone fa-angel"></i> Engineer</div>
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${engineer.id}</li>
-        <li class="list-group-item">Email:<span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
-        <li class="list-group-item">Github Username: <a target="_blank" href="https://github.com/${engineer.githubUsername}"</a></li>
-        </ul>
-        </div>
-            
-         `;
-
-        HTML.push(engineerHtml);
+HTML.push(...teamMembers
+    .filter(teamMembers=>teamMembers.getRole() === "Intern")
+    .map(intern=> renderIntern(intern))
+    );
     
-    }
+    return renderFullMarkdown(HTML.join(""));
 
-     //if Intern was selected from inquirer then this will also get called to generate the internHtml.
-
-    const buildIntern = intern => {
-        console.log(buildIntern);
-        let internHtml = `
-    
-            <div class="d-flex justify-content-around flex-wrap mt-5 m-3">
-            <div class="card-header">
-            ${intern.name} <br/>
-            <i class="fa-duotone fa-dove"></i> Intern</div>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${intern.id}</li>
-            <li class="list-group-item">Email:<span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
-            <li class="list-group-item">School: ${intern.school}</li>
-            </ul>
-            </div>
-    
-            `;
-
-        HTML.push(internHtml);
-    
-
-    }
-
-
-    let moreData = true;
-
-    while (moreData) {
-
-     if (role === "Manager") {
-
-        HTML.push(
-            managerHtml
-            .filter((teamMembers)=>teamMembers.getRole() === "Manager")
-            .map((manager) =>renderManager(manager))
-            );
-              
-    }else if (role === "Engineer") {
-
-        HTML.push(
-            engineerHtml
-            .filter((teamMembers)=>teamMembers.getRole()==="Engineer")
-            .map((engineer)=>renderEngineer(engineer))
-            );
-    }else if (role === "Intern") {
-
-        HTML.push(
-             internHtml
-            .filter((teamMembers)=>teamMembers.getRole() === "Intern")
-            .map((intern)=> renderIntern(intern))
-            );
-
-        }else if (role === "None") {
-
-            moreData = false;
-            HTML.push(generateTeamProfile);
-    }else{
-        return renderFullMarkdown(HTML.join(""));
-    }}
-
-};
-
-   
-    module.exports = members =>{
-
-    
-    return`
-
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-        rel="stylesheet" 
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-       <title> Team Profile Generator</title>
-        </head>
-        <body>
-        <header class="text-center p-4" style="background-color:#05668d !important">
-        <h1> Team Profile </h1>
-        </header>
-
-        <main> ${generateTeamProfile(members)}</main>
-
-        <script defer src="index.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-        crossorigin="anonymous"></script>
-
-        <script src="https://kit.fontawesome.com/8a966edc12.js" crossorigin="anonymous"></script>
-        </body>
-        </html>
-    
-    `
     };
-   
-    
 
+    const renderManager = (manager) => {
+        let template = fs.readFileSync(path.resolve(templatesDir,"manager.html"), "utf8");
+        template = replacePlaceholders(template,"name", manager.getName());
+        template = replacePlaceholders(template,"id", manager.getId());
+        template = replacePlaceholders(template,"email", manager.getEmail());
+        template = replacePlaceholders(template,"OfficeNumber", manager.getOfficeNumber());
+        template = replacePlaceholders(template,"role", manager.getRole());
+        return template;
+    };
+
+    const renderEngineer = (engineer) => {
+        let template = fs.readFileSync(path.resolve(templatesDir,"engineer.html"), "utf8");
+        template = replacePlaceholders(template,"name", engineer.getName());
+        template = replacePlaceholders(template,"id", engineer.getId());
+        template = replacePlaceholders(template,"email", engineer.getEmail());
+        template = replacePlaceholders(template,"githubUsername", engineer.getGithubUsername());
+        template = replacePlaceholders(template,"role", engineer.getRole());
+        return template;
+    };
+
+    const renderIntern = (intern) => {
+        let template = fs.readFileSync(path.resolve(templatesDir,"intern.html"), "utf8");
+        template = replacePlaceholders(template,"name", intern.getName());
+        template = replacePlaceholders(template,"id", intern.getId());
+        template = replacePlaceholders(template,"email", intern.getEmail());
+        template = replacePlaceholders(template,"school", intern.school());
+        template = replacePlaceholders(template,"role", intern.getRole());
+        return template;
+    };
+
+    const renderFullMarkdown = (HTML) => {
+        const templates =fs.readFileSync(path.resolve(templatesDir,"full-markdown.html"),"utf8");
+        return replacePlaceholders(templates, "teamMembers", HTML);
+    };
+
+    const replacePlaceholders = (template, placeholder,value) => {
+        const pattern =new RegExp(`{{${placeholder}}}`, "gm");
+        return template.replace(pattern, value);
+    };
+
+    module.exports = render
--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const inquirer = require('inquirer');
 const Manager = require('./Manager');
 const Engineer = require('./Engineer');
 const Intern = require('./Intern');
-const generateSite = require('./generateSite');
-const path=require("path");
-const OUTPUT_DIR = path.resolve (__dirname,"html")
-const outputPath = path.join(OUTPUT_DIR, "html");
+const render = require('./generateSite');
+
 
 
 const teamMembers = [];
@@ -23,209 +21,183 @@ const validateInput = (response) => {
 
 //initiate the teamProfile building process
 
- const init= async ()=> {
+ function teamMemberQs(){
  
    
 //inquirer prompts the usr with questions to select which team member they would like to enter information
        
-const TeamProfileQs = [
+inquirer.prompt([
                  
             {
-                name: "memberSelection",
-                type: "list",
-                message: "Please choose the module you would like to add to the team profile.",
-                choices: [
-                    { name: "Manager", value: "manager", short: "Manager" },
-                    { name: "Engineer", value: "engineer", short: "Engineer" },
-                    { name: "Intern", value: "intern", short: "Intern" },
-                    { name: "None", value: "none", short: "None" },
-                
-                ],
-                validate: memberSelectionInput = () => {
-                    if (memberSelectionInput) {
-                        return true;
-                    }else{
-                        console.log ('Please select one of these options');
-                        return false;
-                    }}
-                        
+               
+                type:"input",
+                message: "Please enter this person name?",
+                name:"responseName",
+                validate: validateInput,
+            
             },
 
-        ];
-//if the user click on none then the question then it will go to end stage.
+            {
+               
+                type:"input",
+                massage:"Please enter this person id?",
+                name:"responseID",
+                validate: validateInput,
+            },
 
-const{createTeamProfile} = inquirer.prompt(TeamProfileQs);
-createTeamProfile();
-
-if (createTeamProfile === "none"){
-    return builtProfile();
-// if user chooses Manager, engineer, or intern then it will take to the respective process to create selected Member              
-}else{
- if (memberSelectionInput ==="manager") {
-return createManager();
- }
-
-if (memberSelectionInput === "engineer"){
-return createEngineer();
-}
-if(memberSelectionInput === "intern"){
-return createIntern();
-}
-}
-}                
-
-//Above the Team member object is being created via users selection and response/input.
-
-
-//if Manager was to be selected. it will activate the createManager funtion and inqirer will prompt array of questions to gather information on Manager.
-//Similar to above information gathered will then be pushed through the TeamMember array. Call back function will return user to CreateTeamProfile upon completion.
-
-const createManager= async ()=>{
-    
-        const createManagerQs= [
-
-        {
-            type: "input",
-            name: "name",
-            message: "Please enter the person name?",
-            validate: validateInput,
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please enter the person id?",
-            vaildate: validateInput,
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter the person email?",
-            validate: validateInput,
-        },
-        {
-            type: "input",
-            name: "OfficeNumber",
-            message: "Please enter the person office number?",
-            validate: function (value) {
-                let pass = value.match(
-                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
-                if (pass) {
-                    return true;
-                } else {
-                    return "Please enter a valid office number?";
+            {
+               
+                type:"input",
+                message:"Please enter this person Email?",
+                name:"responseEmail",
+                validate:function (value){
+                    let pass =value.match (
+                        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+                    if (pass) {
+                        return true;
+                    }else{
+                        return "Please enter a valid email address";
+                    }
                 }
+            },
+
+            {
+                type: "list",
+                message: "Please choose the person role",
+                name:"responseRole",
+                choices: ["Engineer","Intern", "Manager"],
+            },
+
+        ]).then(function(response) {
+            if (response.responseRole === "Manager"){
+                managerQs(response);
+            }else if (response.responseRole === "Engineer") {
+                engineerQs(response);
+            }else{
+                internQs(response);
             }
-        },
-    ];
+
+            })
+        }
 
 
-const {managerResponse} = inquirer.prompt(createManagerQs);
-const manager = new Manager(managerResponse);
-teamMembers.push(manager);
-createTeamProfile();
-};
+        function managerQs(baseResponse) {
+            inquirer.prompt ([
+                   {
+                       
+                       type: "input",
+                       message: "Please enter the person Office Number?",
+                       name: "responseOfficeNumber",
+                       validate: validateInput
+                   },
+           
+               {
+                   
+                   type:"confirm",
+                   message: "Would you like add another person?",
+                   name:"responseConfirmation",
+               },
+           
+               ]).then(function(response) {
+                   const createManager = new  Manager (baseResponse.responseName, baseResponse.responseID, baseResponse. responseEmail, baseResponse.responseOfficeNumber,baseResponse.responseRole);
+                   teamMembers.push(createManager);
+                   if(response.responseConfirmation === true) {
+                       teamMemberQs()
+                   
+                   }else{
+                       builtTeamProfile();
+                       console.log ("rendered")
+                   }
+               })
+           
+           }
 
-//if Engineer was to be selected. it will activate the createEngineer funtion and inqirer will prompt array of questions to gather information on Engineer.
-//Similar to above information gathered will then be pushed through the TeamMember array. Call back function will return user to CreateTeamProfile upon completion.
 
 
-const createEngineer= async ()=>{
-
-const createEngineerQs =[
-
+   
+ 
+ function engineerQs(baseResponse) {
+ inquirer.prompt ([
         {
+            
             type: "input",
-            name: "name",
-            message: "Please enter the person name?",
-            validate: validateInput,
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please enter the person id?",
-            vaildate: validateInput,
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter the person email?",
-            validate: validateInput,
-        },
-        {
-            type: "input",
-            name: "githubUsername",
             message: "Please enter the person Github Username?",
+            name: "responseGithubUsername",
             validate: function (value) {
                 let pass = value.match(
                     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
                 if (pass) {
                     return true;
                 } else {
-                    return "Please enter a valid Github Username.";
+                    return "Please enter a valid Github Username?";
                 }
             }
+        },
+
+    {
+      
+        type:"confirm",
+        message: "Would you like add another person?",
+        name:"responseConfirmation",
     },
-  ];
 
+    ]).then(function (response) {
+        const createEngineer = new Engineer(baseResponse.responseName, baseResponse.responseID, baseResponse. responseEmail, baseResponse.responseGithubUsername, baseResponse.responseRole);
+        teamMembers.push(createEngineer);
+        if(response.responseConfirmation === true) {
+            teamMemberQs()
+        
+        }else{
+            builtTeamProfile();
+            console.log ("rendered")
+        }
+    })
 
-const engineerResponse = inquirer.prompt(createEngineerQs);
-const engineer = new Engineer(engineerResponse);
-teamMembers.push(engineer);
-createTeamProfile();
-
-
-};
-
-//if Itern was to be selected. it will activate the createEngineer funtion and inqirer will prompt array of questions to gather information on Intern.
-//Similar to above information gathered will then be pushed through the TeamMember array. Call back function will return user to CreateTeamProfile upon completion.
-
-
-const createIntern = async () => {
-
-const createInternQs = [
-
-        {
-            type: "input",
-            name: "name",
-            message: "Please enter the person name?",
-            validate: validateInput,
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please enter the person id?",
-            vaildate: validateInput,
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter the person email?",
-            validate: validateInput,
-        },
-
-        {
-            type: "input",
-            name: "school",
-            message: "Please enter the person school name?",
-            validate: validateInput,
-        },
-    ];
-
-
-const internResponse = inquirer.prompt (createInternQs);
-const intern = new Intern(internResponse);
-teamMembers.push(intern);
-createTeamProfile();
-};
-
-// This fxn here is used to render the teamMember Array and write the file to the html folder.
-
-function builtProfile() {
-    if(!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR)
-    }
-    fs.writeFileSync(outputPath,generateSite(teamMembers),"utf-8");
 }
 
-//call back function to initiate the initial fxn and whole process again
-init();
+function internQs(baseResponse) {
+    inquirer.prompt ([
+           {
+               
+               type: "input",
+               message: "Please enter the person University or college name?",
+               name: "responseSchool",
+               validate: validateInput
+                   
+           },
+   
+       {
+           
+           type:"confirm",
+           message: "Would you like add another person?",
+           name:"responseConfirmation",
+       },
+   
+       ]).then(function(response) {
+           const createIntern = new Intern(baseResponse.responseName, baseResponse.responseID, baseResponse. responseEmail, baseResponse.responseSchool, baseResponse.responseConfirmation, baseResponse.responseRole);
+           teamMembers.push(createIntern);
+           if(response.responseConfirmation === true) {
+               teamMemberQs()
+           
+           }else{
+               builtTeamProfile();
+               console.log ("rendered")
+           }
+       })
+   
+   }
+   function builtTeamProfile() {
+       const HTML =render(teamMembers);
+       fs.writeFileSync("team-profile.html",HTML,(err)=>{
+           if(err) {
+             console.log(err);  
+           }else{
+               console.log("success");
+           }
+       });
+
+    };
+   
+    
+
+teamMemberQs();
